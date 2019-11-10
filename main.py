32067755f21bf1490b80673fe637ea89666b5c8f
Carlos <carlosenmm@gmail.com>
2019-11-10 18:47:42 -0300

    Tabelas encontradas

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 page = requests.get("http://www2.aneel.gov.br/aplicacoes/capacidadebrasil/capacidadebrasil.cfm")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-teste = soup.select("td b")
+# Seleciona as tabelas do site e atribui cada uma a uma posição da lista
+tabelas = soup.select("table")
 
-for item in teste:
-    print (item, "\n")
+# As tabelas de interesse estão todas agrupadas no índice 3 
+indice_tabelas = 3
 
+print(tabelas[indice_tabelas])
 
 '''
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
