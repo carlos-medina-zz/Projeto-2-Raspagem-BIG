49d6a02817970f98bf5dbbb0c0de9ae1fa594740
Carlos <carlosenmm@gmail.com>
2019-11-14 14:13:11 -0300

    Início da transformação das linhas das tabelas em itens de uma lista

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from raspagem import Raspagem
+import numpy as np
+import pandas as pd
 
 info_tabelas, linhas_tabelas = Raspagem()
 
+tabela1 = []
+tabela2 = []
+tabela3 = []
+lista_tabelas = [tabela1, tabela2, tabela3]
+
+
+lista_tabelas[0][0].append("casa")
+
+'''
 for info_tabela in info_tabelas:
+    contador_tabela = 0
+
     for linha in info_tabela:
-        print(linhas_tabelas[linha[0]])
+        contador_linha = 0
+        
+        for item in linhas_tabelas[linha[0]]:
+            lista_tabelas[contador_tabela][contador_linha].append(item)
+
+        contador_linha += 1
+    
+
+    contador_tabela += 1
+'''
