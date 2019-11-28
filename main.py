ee7bb463911a9d10743aabe926b9c07ac02bf317
Carlos <carlosenmm@gmail.com>
2019-11-28 17:31:32 -0300

    Feito a nome do arquivo usando o nome da tabela e a data do dia

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from raspagem import Raspagem
+
 import numpy as np
 import pandas as pd
+from unicodedata import normalize
+from datetime import date
+import os
 
 # Função que retorna duas listas: a primeira contém as três tabelas como dataframes
 # e a segunda os títulos de cada uma das tabelas
-def Cria_dataframe (lista_tabelas):
+def CriaDataframe (lista_tabelas):
     
     # Cria uma lista com 3 listas para armazenar os titulos das tabelas
     titulos_tabelas = [ [] for i in range(len(lista_tabelas))]
@@ -50,10 +54,37 @@ def Cria_dataframe (lista_tabelas):
 
     return (df_list, titulos_tabelas)
 
+# Cria o nome de cada uma das tabelas para ser salvo no disco
+def CriaNomeArquivos(titulos_tabelas):
+    
+    nome_arquivos = [ [] for i in range(len(titulos_tabelas)) ]
+    
+    contador_nome = 0
+    for titulo in titulos_tabelas:
+
+        # Troca os espaços dos nomes por _
+        nome_arquivos[contador_nome] = titulo.replace(" ", "_")
+
+        # Substitui caracteres acentuados pelos seus equivalentes não acentudos
+        nome_arquivos[contador_nome] = normalize('NFKD', nome_arquivos[contador_nome]).encode('ASCII', 'ignore').decode('ASCII')
+
+        # Adiciona a data atual no nome do arquivo
+        today = date.today()
+        today = str(today)
+        nome_arquivos[contador_nome] = nome_arquivos[contador_nome] + "_" + today
+
+        contador_nome += 1
+
+    return (nome_arquivos)
+
+# Código principal
+
+# Definição do local onde serão asmazenados os arquivos .csv
+local = "C:\Dados_BIG"
 
 lista_tabelas = Raspagem()
 
-df_list, titulos_tabelas = Cria_dataframe(lista_tabelas)
-print(df_list[0])
-print(df_list[1])
-print(df_list[2])
+df_list, titulos_tabelas = CriaDataframe(lista_tabelas)
+
+nome_arquivos = CriaNomeArquivos(titulos_tabelas)
+print(os.listdir(local))
